95d1b7fd13121787394c6c2070e2c2fe1d037bd9
Meeshaan Shah <shah179@mail.chapman.edu>
2016-06-02 14:43:47 -0700

    Create repo

---
+++ include/Q.h
@@ -0,0 +1,21 @@
+//Meeeshaan Shah
+//CPSC 350
+//Assigment 3
+//Generic Queue interface for implemention of a queue class
+
+#ifndef Q_H
+#define Q_H
+
+template <typename T>
+class Q
+{
+	public:
+		virtual void Enqueue(T d) = 0;
+		virtual void Dequeue() = 0;
+		virtual T getQueueFront() = 0;
+		virtual bool isQueueEmpty() = 0;
+		virtual int getQueueSize() = 0;
+		//virtual void printQueue() = 0;
+};
+
+#endif
